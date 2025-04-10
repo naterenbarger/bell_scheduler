8245345937d2826c875ff029e4aebf8735084580
Nate Renbarger <nate.renbarger@gmail.com>
2025-04-10 18:45:55 -0400

    Remove unnecessary migration file

--- backend/internal/store/migrations/20240327120000_remove_smtp_columns.sql
+++
@@ -1,6 +0,0 @@
--- Remove SMTP columns from settings table
-ALTER TABLE settings DROP COLUMN smtp_host;
-ALTER TABLE settings DROP COLUMN smtp_port;
-ALTER TABLE settings DROP COLUMN smtp_username;
-ALTER TABLE settings DROP COLUMN smtp_password;
-ALTER TABLE settings DROP COLUMN smtp_from; 
